ba711a430620cced4d42a09387d3c8abf89e0dc3
thijs <Thijsvanmarle@gmail.com>
2017-12-07 22:01:50 +0100

    basic routing | side menu added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import {Routes, RouterModule} from '@angular/router';
+import { ServersComponent } from './body-container/servers/servers.component';
+import { DashboardComponent } from './body-container/dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyContainerComponent } from './body-container/body-container.component';
+
+const appRoutes: Routes = [
+  {path: 'servers', component: ServersComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', component: DashboardComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ServersComponent,
+    DashboardComponent,
+    HeaderComponent,
+    BodyContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
